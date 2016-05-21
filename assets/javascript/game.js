ec99fdaf69920b1957f3413f19f1b9dd127859c0
Coffeeteer <tgrieshiemer@gmail.com>
2016-05-21 13:22:42 -0400

    received help, now almost done.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,48 @@
 document.addEventListener("DOMContentLoaded",function(){
 
-var wins = document.getElementById("wins");
-var losses = document.getElementById("losses");
+var winSpan = document.getElementById("wins");
+var winNum =  0;
+
+var loseSpan = document.getElementById("losses");
+var loseNum = 0;
+
 var guessesLeft = document.querySelector("#guessLeft");
 var playerguess = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 var guessVal = document.getElementById("guess_val");
+var letterArray= [];
+var random = "ABCDEFGHIJKLMNOPQURSTUVWXYZ".split('');
 //input where letter is typed
-var guessLetter = document.getElementById("guess_letter");
+// var guessLetter = document.getElementById("guess_letter");
 
 
 	//move letter from imput to <p> tag
 	document.onkeyup = function letterPick(event){
 		console.log(guessVal);
-		var guessLetter = String.fromCharCode(event.keyCode).toUpperCase();
-		//document.createElement(getElementById("guess_val"));
-		guessVal.innerHTML = guessLetter;
-		//guessLetter.str.substring(0,9);
-		console.log(guessVal);
-
+		var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+		//document.createElement(getElementById("guess_val")
+		letterArray.push(userGuess);
+		guessVal.innerHTML = letterArray;
+		guessLetter = "";
+		// guessLetter.str.substring(0,9);
+		console.log(userGuess);
+		var computerGuess = cpuGuess();
+		console.log(computerGuess);
+		console.log(winNum);
+		
+		if(userGuess === computerGuess){
+			winNum ++;
+			winSpan.innerHTML = winNum;
+		}else{
+			loseNum ++;
+			loseSpan.innerHTML = loseNum;
+			console.log('You guessed wrong');
+		}
 	}
 
 //generate cpuGuess
 function cpuGuess (){
-	var random = "ABCDEFGHIJKLMNOPQURSTUVWXYZ".split('');
-	return str.substring(Math.floor(Math.random() * 26), 1);
+	var randomIndex = Math.floor(Math.random()* random.length);
+	 return random[randomIndex];	
 }
 
 function guessStr(guessVal, guessLetter){
@@ -31,11 +50,7 @@ function guessStr(guessVal, guessLetter){
 		if(i === guessVal && i === guessLetter){
 			guessVal.innerHTML.str.substr(0, 9);
 			guessLetter.innerHTML.str.substr(0, 9);
-			 if(wins ===){
-			 	//not guessed keep going - 8 more turns
-			 } else if {
-			 	//
-			 }
+			 
 		}
 	}
 }
